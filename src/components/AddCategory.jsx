cbc0b76a5305a45c08fc7a548227e7e3aad6db2f
Jeldry Pozo <jfcdonfer03@gmail.com>
2024-01-31 20:40:33 -0400

    3rd commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+
+
+export const AddCategory = ({onNewCategory})=>{
+
+ const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = ({target})=>{
+    // console.log(target.value) //
+    setInputValue(event.target.value);
+
+  }
+
+     const onSubmit = (event)=>{
+     event.preventDefault();
+ 
+       if(inputValue.trim().length <= 1) return;
+        setInputValue('');
+        onNewCategory( inputValue.trim())
+     // console.log(inputValue) //
+      // setCategories(categories => [inputValue, ...categories]) //
+     }
+
+
+ return(
+   <form onSubmit={onSubmit}>
+  <input type="text"
+  placeholder="Buscar Gifs"
+  value={inputValue} 
+  onChange={onInputChange}
+
+  />
+  </form>
+ )
+
+
+}
